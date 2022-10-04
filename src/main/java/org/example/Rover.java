5099f99084db253fc82590decd848aa20584ed90
Anshuman Singh <41586858+Anshuman-37@users.noreply.github.com>
2022-10-04 14:18:38 +0100

    Refactered the moving forward procedure

--- src/main/java/org/example/Rover.java
+++ src/main/java/org/example/Rover.java
@@ -43,15 +43,7 @@ public class Rover {
 
 
         else if( getInstruction().equals("F") ){
-            if(getDirection().equals("West"))
-                this.x = this.x-1;
-            else if(getDirection().equals("East"))
-                this.x = this.x+1;
-            else if(getDirection().equals("North"))
-                this.y = this.y+1;
-            else if(getDirection().equals("South"))
-                this.y = this.y-1;
-            this.coordinates = Integer.toString(this.x)+","+Integer.toString(this.y);
+            moveForward();
         }
 
         else if( getInstruction().equals("B") ){
@@ -65,6 +57,18 @@ public class Rover {
                 this.y = this.y+1;
             this.coordinates = Integer.toString(this.x)+","+Integer.toString(this.y);
         }
+    }
+
+    private void moveForward() {
+        if(getDirection().equals("West"))
+            this.x = this.x-1;
+        else if(getDirection().equals("East"))
+            this.x = this.x+1;
+        else if(getDirection().equals("North"))
+            this.y = this.y+1;
+        else if(getDirection().equals("South"))
+            this.y = this.y-1;
+        this.coordinates = Integer.toString(this.x)+","+Integer.toString(this.y);
     }
 
 
